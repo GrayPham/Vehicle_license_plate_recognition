da64a4932783e04ce0338621632438f2667ec755
Tam Nhu <tnhubh@gmail.com>
2022-04-16 17:27:40 +0700

    16/04: Tạo form login và kết nối database bằng linq

---
+++ Vehicle_license_plate_recognition/FormLogin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vehicle_license_plate_recognition.Model; 
+
+namespace Vehicle_license_plate_recognition
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+        LOGIN test = new LOGIN();
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_username.Text) || string.IsNullOrEmpty(textBox_pass.Text))
+            {
+                MessageBox.Show("Please provide your Username and Password!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_username.Select();
+                return;
+            }
+            try
+            {
+                //var q = test.LOGINs.Where(user => user.Username == textBox_username.Text && user.Password == textBox_pass.Text).First();
+                var q = (from p in LOGINs
+                             where p.Username == textBox_username.Text && p.Password == textBox_pass.Text select p).First();
+
+                Form1 a = new Form1();
+                this.Visible = false; //ẩn thôi chứ khum tắt
+                a.ShowDialog();
+                this.Visible = true; //mở lại
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Please check your Username and Password again!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_username.Select();
+                return;
+
+            }
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label_forgot_Click(object sender, EventArgs e)
+        {
+            FormForgotPassword a = new FormForgotPassword();
+            this.Visible = false;
+            a.ShowDialog();
+            this.Visible = true;
+        }
+    }
+}
--- Vehicle_license_plate_recognition/Program.cs
+++ Vehicle_license_plate_recognition/Program.cs
@@ -16,7 +16,7 @@ namespace Vehicle_license_plate_recognition
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormLogin());
         }
     }
 }
